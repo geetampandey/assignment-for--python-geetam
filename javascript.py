04fdcd35680bac8889418e5560cad2246115a280
geetampandey <gns.bt.90@gmail.com>
2026-05-05 14:04:01 +0530

    assignment

--- javascript.py
+++ javascript.py
@@ -11,3 +11,4 @@ char1=char2
 char2=char3
 print(f"output the value of char1={char1}")
 print(f"output the vlue of char2={char2}")
+print()
---
+++ que3.py
@@ -0,0 +1,11 @@
+# Switch one string value with one integer value
+#input
+n1 = 200,
+char2 = "java"
+print(f"input value of n1 ={n1}")
+print(f"input value of char2 ={char2}")
+n3=n1
+n1=char2
+char2=n1
+print(f"output the value of n1={n1}")
+print(f"output the vlue of char2={char2}")
