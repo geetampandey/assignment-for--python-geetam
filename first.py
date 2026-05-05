2c652f803e8f7e2e99716222b9eeb9020c97a59e
geetampandey <gns.bt.90@gmail.com>
2026-05-05 11:16:50 +0530

    assignment

---
+++ first.py
@@ -0,0 +1,12 @@
+#1. Switch values of two integers
+#Input:
+n1 = 20
+n2 = 30
+print(f"input the value of n1 = {n1}")
+print(f"input the value of n2 = {n2}")
+print("-"*20)
+n3=n1
+n1=n2
+n2=n3
+print(f"output the value of n1 = {n1}")
+print(f"output the value of n2 = {n2}")
