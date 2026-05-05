798655075664de4c728b9bfb17aea819e6df996a
geetampandey <gns.bt.90@gmail.com>
2026-05-05 13:28:47 +0530

    practice

---
+++ javascript.py
@@ -0,0 +1,13 @@
+#Switch values of two strings (characters)
+#input
+char1 = "hello"
+char2= "java"
+print(f"input value of char1 ={char1}")
+print(f" input value of char2 ={char2}")
+
+print("-"*20)
+char3=char1
+char1=char2
+char2=char3
+print(f"output the value of char1={char1}")
+print(f"output the vlue of char2={char2}")
