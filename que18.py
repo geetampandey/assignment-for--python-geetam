43fa7ab5f8c6edeec068a8a48c1844aa456f0e9a
geetampandey <gns.bt.90@gmail.com>
2026-05-06 23:30:06 +0530

    geetam

---
+++ que18.py
@@ -0,0 +1,13 @@
+#Convert 20°C to Fahrenheit
+#Input: 20°C
+#Output: ?
+
+
+# Input
+celsius = 20
+
+# Conversion formula
+fahrenheit = (celsius * 9/5) + 32
+
+# Output
+print("Temperature in Fahrenheit:", fahrenheit)
---
+++ que19.py
@@ -0,0 +1,11 @@
+#Calculate area of a triangle
+#Input: base = 10, height = 6
+#Output: Area = ?
+
+
+base = 10
+height = 6
+
+area = 0.5 * base * height
+
+print("Area =", area)
